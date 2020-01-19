e4785d33b8b221cca05b036e3e521dc812e8bf8f
Ravenyy <ravenyy@gmail.com>
2020-01-19 18:25:32 +0100

    respondtoshuffle

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,43 +1,53 @@
 require_relative '../lib/card'
 
 RSpec.describe Card do
-    before do
-        suit = "Diamonds"
-        rank = "8"
+    describe '#new' do
+    let(:suit) { "Diamonds" }
+    let(:rank) { "8" }
 
-        @card = Card.new(suit, rank)
-    end
-    
-    it "responds to suit" do
-        expect(@card).to respond_to(:suit)
+    subject(:card) { Card.new suit, rank}
+      
+    it "adds new card" do
+        expect{card}.not_to raise_error
     end
 
+    it"responds to suit" do
+        expect(card).to respond_to(:suit)
+    end
+        
     it "responds to rank" do
-        expect(@card).to respond_to(:rank)
+        expect(card).to respond_to(:rank)
     end
 
-    it "responds to show" do
-        expect(@card).to respond_to(:show)
+    it "returns correct suit value" do
+        expect(card.suit).to eq("Diamonds")
     end
 
-    it "'show' method returns 'true'" do
-        expect(@card.show).to eq(true)
+
+    it "returns correct rank value" do
+        expect(card.rank).to eq("8")
     end
 
-    it "'suit' method returns 'Diamonds'" do
-        expect(@card.suit).to eq('Diamonds')
+    it "responds to 'show' method" do
+        expect(card).to respond_to(:show)
     end
 
-    it "'rank' method returns '8'" do
-        expect(@card.rank).to eq('8')
+    it "show method returns 'true'" do
+        expect(card.show).to eq(true)
+    end
+end
+
+    describe "#to_s" do
+    subject(:card) { Card.new "Diamonds", "8"}
+
+    it "returns correct output if 'show' is true" do
+        expect(card.to_s).to be_a(String).and eq("8 of Diamonds")
     end
 
-    it "output rank and suit if 'show' is true" do 
-        expect("#{@card}").to eq("#{@card.rank} of #{@card.suit}")
+    it "returns empty string if 'show' is false" do
+        card.show = false
+        expect(card.to_s).to be_a(String).and eq("")
     end
 
-    it "does not output anything if 'show' is false" do
-        @card.show = false
-        expect("#{@card}").to eq("")
-    end
+end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,53 +1,78 @@
 require_relative '../lib/deck'
 
 RSpec.describe Deck do
+    describe "#new" do
     SUITS = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
     RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 
-    before do
-        @deck = Deck.new(SUITS, RANKS)
+    let(:deck) { 
+        double(Deck) }
+    subject { Deck.new(deck)}
+
+    it "creates new deck" do
+        expect{deck}.not_to raise_error
     end
 
-    it "responds to deck" do
-        expect(@deck).to respond_to(:deck)
+    it "receives suits" do
+        allow(deck).to receive(:suits)
     end
 
-    it "responds to suits" do
-        expect(@deck).to respond_to(:suits)
-    end
-
-    it "responds to ranks" do
-        expect(@deck).to respond_to(:ranks)
+    it "receives ranks" do
+        allow(deck).to receive(:ranks)
     end
 
     it "responds to shuffle" do
-        expect(@deck).to respond_to(:shuffle)
+        expect(deck).to respond_to(:shuffle)
     end
+end
 
-    it "responds to deal_card" do
-        expect(@deck).to respond_to(:deal_card)
-    end
+    # SUITS = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+    # RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 
-    it "responds to replace_with" do 
-        expect(@deck).to respond_to(:replace_with)
-    end
+    # before do
+    #     @deck = Deck.new(SUITS, RANKS)
+    # end
 
-    it "pops off the stack when a card is dealt" do
-        dealt_card = @deck.shuffle.last
-        expect(@deck.deal_card).to eq(dealt_card)
-    end
+    # it "responds to deck" do
+    #     expect(@deck).to respond_to(:deck)
+    # end
 
-    it "dealt card can't be nil" do
-        expect(@deck.deal_card).not_to eq(nil)
-    end
+    # it "responds to suits" do
+    #     expect(@deck).to respond_to(:suits)
+    # end
 
-    it "gets a new deck with replace_with" do
-        deck_of_cards = []
-        deck_of_cards.push(Card.new('Clubs', '2'))
-        deck_of_cards.push(Card.new('Clubs', '3'))
-        new_deck = @deck.dup
-        new_deck.replace_with(deck_of_cards)
-        expect(@deck.deck).not_to eq(new_deck.deck)
-    end
+    # it "responds to ranks" do
+    #     expect(@deck).to respond_to(:ranks)
+    # end
+
+    # it "responds to shuffle" do
+    #     expect(@deck).to respond_to(:shuffle)
+    # end
+
+    # it "responds to deal_card" do
+    #     expect(@deck).to respond_to(:deal_card)
+    # end
+
+    # it "responds to replace_with" do 
+    #     expect(@deck).to respond_to(:replace_with)
+    # end
+
+    # it "pops off the stack when a card is dealt" do
+    #     dealt_card = @deck.shuffle.last
+    #     expect(@deck.deal_card).to eq(dealt_card)
+    # end
+
+    # it "dealt card can't be nil" do
+    #     expect(@deck.deal_card).not_to eq(nil)
+    # end
+
+    # it "gets a new deck with replace_with" do
+    #     deck_of_cards = []
+    #     deck_of_cards.push(Card.new('Clubs', '2'))
+    #     deck_of_cards.push(Card.new('Clubs', '3'))
+    #     new_deck = @deck.dup
+    #     new_deck.replace_with(deck_of_cards)
+    #     expect(@deck.deck).not_to eq(new_deck.deck)
+    # end
     
 end
